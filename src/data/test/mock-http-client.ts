ed553b96b542be7911b123091289d1e3391feb01
GuiFSs <devguisantos@gmail.com>
2020-07-21 09:42:19 -0300

    test: ensure RemoteAuthentication calls HttpPostClient with correct body

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -2,9 +2,12 @@ import { HttpPostClient } from 'data/protocols/http/http-post-client'
 
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string
+  body?: object
+
   async post (params: HttpPostClient.Params): Promise<void> {
-    const { url } = params
+    const { url, body } = params
     this.url = url
+    this.body = body
     return Promise.resolve()
   }
 }
