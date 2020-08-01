86ec35cd6e41653f866c9148700dac24a7d3ca59
GuiFSs <devguisantos@gmail.com>
2020-07-31 22:45:55 -0300

    test: ensure ValidationBuilder returns a list o validations

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,19 +1,33 @@
 import { ValidationBuilder as sut } from './validation-builder'
 import { RequiredFieldValidation, EmailValidation, MinLengthValidation } from '@/validation/validators'
+import faker from 'faker'
 
 describe('ValidationBuilder', () => {
+  const field = faker.database.column()
+
   test('Should return RequiredFieldValidation', () => {
-    const validations = sut.field('any_field').required().build()
-    expect(validations).toStrictEqual([new RequiredFieldValidation('any_field')])
+    const validations = sut.field(field).required().build()
+    expect(validations).toStrictEqual([new RequiredFieldValidation(field)])
   })
 
   test('Should return EmailValidation', () => {
-    const validations = sut.field('any_field').email().build()
-    expect(validations).toStrictEqual([new EmailValidation('any_field')])
+    const validations = sut.field(field).email().build()
+    expect(validations).toStrictEqual([new EmailValidation(field)])
   })
 
   test('Should return MinLengthValidation', () => {
-    const validations = sut.field('any_field').min(5).build()
-    expect(validations).toStrictEqual([new MinLengthValidation('any_field', 5)])
+    const length = faker.random.number()
+    const validations = sut.field(field).min(length).build()
+    expect(validations).toStrictEqual([new MinLengthValidation(field, length)])
+  })
+
+  test('Should return a list of validations', () => {
+    const length = faker.random.number()
+    const validations = sut.field(field).required().email().min(length).build()
+    expect(validations).toStrictEqual([
+      new RequiredFieldValidation(field),
+      new EmailValidation(field),
+      new MinLengthValidation(field, length)
+    ])
   })
 })
