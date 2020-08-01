403c8660d451dc02007bf6775e91967d52704935
GuiFSs <devguisantos@gmail.com>
2020-07-31 22:37:30 -0300

    fix: tests to compare with strict equal

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,14 +1,14 @@
-import { RequiredFieldValidation } from '@/validation/validators'
 import { ValidationBuilder as sut } from './validation-builder'
+import { RequiredFieldValidation, EmailValidation } from '@/validation/validators'
 
 describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
     const validations = sut.field('any_field').required().build()
-    expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+    expect(validations).toStrictEqual([new RequiredFieldValidation('any_field')])
   })
 
   test('Should return EmailValidation', () => {
     const validations = sut.field('any_field').email().build()
-    expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+    expect(validations).toStrictEqual([new EmailValidation('any_field')])
   })
 })
