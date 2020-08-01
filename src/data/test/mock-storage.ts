1a9745bf7426b929223affe22fd0c90c27f4d011
GuiFSs <devguisantos@gmail.com>
2020-08-01 16:01:19 -0300

    refactor: rename SetStorageSpy to SetStorageMock

--- src/data/test/mock-storage.ts
+++ src/data/test/mock-storage.ts
@@ -1,5 +1,5 @@
 import { SetStorage } from '@/data/protocols/cache/set-storage'
-export class SetStorageSpy implements SetStorage {
+export class SetStorageMock implements SetStorage {
   key: string
   value: any
   async set (key: string, value: any): Promise<void> {
