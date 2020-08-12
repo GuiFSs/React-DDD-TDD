47400c14a33d4d3bcf029cf1d4dff60a8694b3ea
GuiFSs <devguisantos@gmail.com>
2020-08-12 00:36:27 -0300

    test: ensure CurrentAccountAdapter throws if an invalid account is provided

--- src/main/adapters/current-account-adapter.spec.ts
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -1,6 +1,7 @@
 import { mockAccountModel } from '@/domain/test'
 import { setCurrentAccountAdapter } from './current-account-adapter'
 import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
+import { UnexpectedError } from '@/domain/errors'
 
 // will mock internal methods to be dummies, so it doesn't really save on localstorage (in this case)
 // it doesn't care if the called method will do it's job, because they are dummies
@@ -12,5 +13,11 @@ describe('CurrentAccountAdapter', () => {
     const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
     setCurrentAccountAdapter(account)
     expect(setSpy).toHaveBeenCalledWith('account', account)
+  })
+
+  test('Should throw UnexpectedError ', () => {
+    expect(() => {
+      setCurrentAccountAdapter(undefined)
+    }).toThrow(new UnexpectedError())
   })
 })
