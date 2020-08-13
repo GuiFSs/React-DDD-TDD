b666d1a4429fb1bfd83f7d74d483b464c6f8b1a9
GuiFSs <devguisantos@gmail.com>
2020-08-13 15:00:56 -0300

    refactor: remove unused code

--- src/main/adapters/current-account-adapter.spec.ts
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -1,7 +1,6 @@
 import { mockAccountModel } from '@/domain/test'
 import { setCurrentAccountAdapter, getCurrentAccountAdapter } from './current-account-adapter'
 import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
-import { UnexpectedError } from '@/domain/errors'
 
 // will mock internal methods to be dummies, so it doesn't really save on localstorage (in this case)
 // it doesn't care if the called method will do it's job, because they are dummies
@@ -13,12 +12,6 @@ describe('CurrentAccountAdapter', () => {
     const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
     setCurrentAccountAdapter(account)
     expect(setSpy).toHaveBeenCalledWith('account', account)
-  })
-
-  test('Should throw UnexpectedError ', () => {
-    expect(() => {
-      setCurrentAccountAdapter(undefined)
-    }).toThrow(new UnexpectedError())
   })
 
   test('Should call LocalStorageAdapter.get with correct value', () => {
