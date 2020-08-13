71667f79e4c085250ffe94e595279d0f6f6aefcf
GuiFSs <devguisantos@gmail.com>
2020-08-13 17:44:52 -0300

    test: ensure SurveyList logout on AccessDeniedError

--- src/main/test/cypress/support/survey-list-mocks.ts
+++ src/main/test/cypress/support/survey-list-mocks.ts
@@ -2,3 +2,4 @@ import * as Http from './http-mock'
 
 const url = /surveys/
 export const mockUnexpectedError = (): void => Http.mockServerError(url, 'GET')
+export const mockAccessDeniedError = (): void => Http.mockForbiddenError(url, 'GET')
