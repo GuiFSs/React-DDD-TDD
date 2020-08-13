3dad87d6ef9a1d77b464a27f91026bbd0bcca65d
GuiFSs <devguisantos@gmail.com>
2020-08-13 15:53:14 -0300

    feat: ensure SurveyList logout on AccessDeniedError

--- src/presentation/pages/SurveyList/index.tsx
+++ src/presentation/pages/SurveyList/index.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import Styles from './styles.scss'
 import Footer from '@/presentation/components/Footer'
 import Header from '@/presentation/components/Header'
 import { SurveyListItem, SurveyError } from './components'
 import { LoadSurveyList } from '@/domain/usecases'
 import SurveyContext from './context/context'
+import { AccessDeniedError } from '@/domain/errors'
+import { useHistory } from 'react-router-dom'
+import { ApiContext } from '@/presentation/contexts'
 
 interface Props {
   loadSurveyList: LoadSurveyList
@@ -17,6 +20,8 @@ interface State {
 }
 
 const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
+  const { setCurrentAccount } = useContext(ApiContext)
+  const history = useHistory()
   const [state, setState] = useState<State>({
     surveys: [],
     error: '',
@@ -26,7 +31,14 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
   useEffect(() => {
     loadSurveyList.loadAll()
       .then(surveys => setState(prev => ({ ...prev, surveys })))
-      .catch(err => setState(prev => ({ ...prev, error: err.message })))
+      .catch(err => {
+        if (err instanceof AccessDeniedError) {
+          setCurrentAccount(undefined)
+          history.replace('/login')
+        } else {
+          setState(prev => ({ ...prev, error: err.message }))
+        }
+      })
   }, [state.reload])
 
   return (
