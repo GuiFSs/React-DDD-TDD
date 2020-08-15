168772920f7cd0f944feb2107b21b1b6db816443
GuiFSs <devguisantos@gmail.com>
2020-08-15 00:26:23 -0300

    test: ensure SurveyResult logout if request returns 403

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -2,7 +2,9 @@ import * as Helper from '../utils/helpers'
 import * as Http from '../utils/http-mocks'
 
 const path = /surveys/
+const visitUrl = '/surveys/any_id'
 const mockUnexpectedError = (): void => Http.mockServerError(path, 'GET')
+const mockAccessDeniedError = (): void => Http.mockForbiddenError(path, 'GET')
 const mockSuccess = (): void => Http.mockOk(path, 'GET', 'fx:survey-result')
 
 describe('SurveyResult', () => {
@@ -14,16 +16,22 @@ describe('SurveyResult', () => {
 
   it('Should present error on UnexpectedError', () => {
     mockUnexpectedError()
-    cy.visit('/surveys/any_id')
+    cy.visit(visitUrl)
     cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
   })
 
   it('Should reload on button click', () => {
     mockUnexpectedError()
-    cy.visit('/surveys/any_id')
+    cy.visit(visitUrl)
     cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
     mockSuccess()
     cy.getByTestId('reload').click()
     cy.getByTestId('question').should('exist')
+  })
+
+  it('Should logout on AccessDeniedError', () => {
+    mockAccessDeniedError()
+    cy.visit(visitUrl)
+    Helper.testUrl('/login')
   })
 })
